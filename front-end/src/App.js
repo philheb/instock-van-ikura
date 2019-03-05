2debed8bacd9abb00629a97b7fe973509f353656
philheb <tom@toms-MacBook-Pro.local>
2019-03-04 17:16:36 -0800

    Added warehouse

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-        </header>
+        <header className="App-header" />
       </div>
     );
   }
