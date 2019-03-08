efef1a43fe04c4fad2d4904b1ddf807c097a934c
Karjen Wong <karjenwong@gmail.com>
2019-03-08 11:17:48 -0800

    sass is mucking things up

--- front-end/src/Components/InventoryItem/InventoryItem.js
+++ front-end/src/Components/InventoryItem/InventoryItem.js
@@ -4,37 +4,40 @@ import { Link } from 'react-router-dom'
 import "./InventoryItem.css"
 
 class InventoryItem extends React.Component {
-state = {
-  className: "hide",
-  
-}
+  state = {
+    className: "hide",
+
+  }
   toggleClass = () => {
     if (this.hide.className === "hide") {
-      this.setState({className: 'show'})      
+      this.setState({ className: 'show' })
     } else {
-      this.setState({className: 'hide'})
+      this.setState({ className: 'hide' })
     }
+  }
+
+  deleteItem = () => {
+    console.log('hi')
   }
 
   render() {
     return (
       <tr>
         <td data-label="ITEM">
-        <Link id="link__color--black" to={`/inventory/${this.props.data.id}`}>
-          {this.props.data.item}
-          <br />
-          <p className="description__font">{this.props.data.description}</p>
+          <Link id="link__color--black" to={`/inventory/${this.props.data.id}`}>
+            {this.props.data.item}
+            <br />
+            <p className="description__font">{this.props.data.description}</p>
           </Link>
         </td>
-
         <td data-label="LAST ORDERED">{this.props.data.lastOrdered}</td>
         <td data-label="LOCATION">{this.props.data.location}</td>
         <td data-label="QUANTITY">{this.props.data.quantity}</td>
         <td data-label="STATUS">{this.props.data.status}</td>
         <td data-label="DOT">
           <div className="dropdown" onClick={this.toggleClass}>
-          <img src={dot} alt=""  className="pointer"/>
-            <div id ="closeAll"className={this.state.className} ref={self => this.hide = self} name={this.props.data.id}>Remove</div>
+            <img src={dot} alt="" className="pointer" />
+            <div className={this.state.className} ref={self => this.hide = self} name={this.props.data.id} onClick={this.deleteItem}>Remove</div>
           </div>
 
         </td>
