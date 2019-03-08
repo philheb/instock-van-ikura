8c8c6cbab6f3dc16c5ee2160f29406d419284025
Vicky Wong <vickywg998@hotmail.com>
2019-03-08 14:09:09 -0800

    hooked up inventory item detail

--- front-end/src/Components/InventoryItemDetail/InventoryItemDetail.js
+++ front-end/src/Components/InventoryItemDetail/InventoryItemDetail.js
@@ -3,57 +3,105 @@ import { Link } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import backArrow from '../../Assets/Icons/Icon-back-arrow.svg'
 import './InventoryItemDetail.scss'
+import axios from 'axios'
 
-export default function WarehouseDetails() {
-  return (
-    <div className="locationDetails">
-      <Navbar />
-      <div className="inventoryDetail__container">
-        <div className="inventoryDetail__font--wrapper">
-          <Link to="/inventory">
-            <img src={backArrow} alt="back-arrow" />
-          </Link>
-          <h1 className="inventoryDetail__font">Product Name</h1>
-          <button id="instock__button">In Stock</button>
-        </div>
-        <div className="inventoryDetail__wrapper">
-          <div className="inventoryDetail__top--section">
-            <h5 className="locationDetails__label">ITEM DESCRIPTION</h5>
-            <h4 className="locationDetails__body--top">Here is a more detailed summary of the product name, it’s uses, industries and
-possible attributes that could be used to describe the product.</h4>
-          </div>        
-          <div className ="inventoryDetail__section--wrapper">
-          <div className="inventoryDetail__top--section">
-            <h5 className="inventoryDetails__label">ORDERED BY</h5>
-            <h4 className="inventoryDetails__body--top">
-              Mark Saunders
-            </h4>
-            <h5 className="inventoryDetails__label">LAST ORDERED</h5>
-            <h4 className="inventoryDetails__body--top">
-              2018-05-24
-            </h4>
-            <h5 className="inventoryDetails__label">QUANTITY</h5>
-            <h4 className="inventoryDetails__body--top">
-              12000
-            </h4>
-            <h5 className="inventoryDetails__label">CATEGORIES</h5>
-            <h4 className="inventoryDetails__body--top">
-            Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation, Sales
-            </h4>
-           
+const inventoryItemURL = `http://localhost:8080/inventory/`
+const inventoryItemDetailURL = id => `http://localhost:8080/inventory/${id}`
+
+class InventoryItemDetails extends React.Component {
+
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    axios.get(inventoryItemURL)
+      .then(response => {
+        this.setState({
+          data: response.data,
+          inventoryID: response.data[0].id
+        })
+      }).then(response => {
+        axios.get(inventoryItemDetailURL(this.state.inventoryID))
+          .then(response => {
+            const { description, name, id, item, lastOrdered,
+              location, quantity, status, referenceNumber } = response.data
+            this.setState({
+              description: description,
+              id: id,
+              item: item,
+              lastOrdered: lastOrdered,
+              location: location,
+              quantity: quantity,
+              status: status,
+              name: name,
+              referenceNumber: referenceNumber
+            });
+          });
+      });
+  }
+
+  render() {
+
+    return (
+      <div className="locationDetails">
+        <Navbar />
+        <div className="inventoryDetail__container">
+          <div className="inventoryDetail__font--wrapper">
+            <Link to="/inventory">
+              <img src={backArrow} alt="back-arrow" />
+            </Link>
+            <h1 className="inventoryDetail__font">
+              {this.state.item}
+            </h1>
+            <button id="instock__button">
+              {this.state.status}
+            </button>
           </div>
-          <div className="inventoryDetail__top--section">
-            <h5 className="inventoryDetails__label">REFERENCE NUMBER</h5>
-            <h4 className="inventoryDetails__body--top">JK2020FD7811201</h4>
-            <h5 className="inventoryDetails__label">LOCATION</h5>
-            <h4>Toronto, CAN</h4>
+          <div className="inventoryDetail__wrapper">
+            <div className="inventoryDetail__top--section">
+              <h5 className="locationDetails__label">ITEM DESCRIPTION</h5>
+              <h4 className="locationDetails__body--top">
+                {this.state.description}
+              </h4>
+            </div>
+            <div className="inventoryDetail__section--wrapper">
+              <div className="inventoryDetail__top--section">
+                <h5 className="inventoryDetails__label">ORDERED BY</h5>
+                <h4 className="inventoryDetails__body--top">
+                  {this.state.name}
+                </h4>
+                <h5 className="inventoryDetails__label">LAST ORDERED</h5>
+                <h4 className="inventoryDetails__body--top">
+                  {this.state.lastOrdered}
+                </h4>
+                <h5 className="inventoryDetails__label">QUANTITY</h5>
+                <h4 className="inventoryDetails__body--top">
+                  {this.state.quantity}
+                </h4>
+                <h5 className="inventoryDetails__label">CATEGORIES</h5>
+                <h4 className="inventoryDetails__body--top">
+                  Industrial, Automotive, Heavy, Mechanical, 
+                  Engineering, Transportation, Sales
+            </h4>
+              </div>
+              <div className="inventoryDetail__top--section">
+                <h5 className="inventoryDetails__label">REFERENCE NUMBER</h5>
+                <h4 className="inventoryDetails__body--top">
+                  {this.state.referenceNumber}
+                </h4>
+                <h5 className="inventoryDetails__label">LOCATION</h5>
+                <h4>{this.state.location}</h4>
+              </div>
+            </div>
           </div>
-          </div>
-        </div>
-        <div className="inventoryDetail__wrapper--edit">
+          <div className="inventoryDetail__wrapper--edit">
             <button id="inventoryDetail__edit--button">EDIT</button>
           </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default InventoryItemDetails;
