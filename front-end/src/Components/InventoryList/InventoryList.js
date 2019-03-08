9a5d314e46ef335d22102ef6a0c7fa3fc3d071ce
philheb <tom@android-c9550bb7b6ec7fbc.localdomain>
2019-03-08 14:28:17 -0800

    Added UI for adding inventory item

--- front-end/src/Components/InventoryList/InventoryList.js
+++ front-end/src/Components/InventoryList/InventoryList.js
@@ -2,24 +2,39 @@ import React from 'react'
 import InventoryItem from '../InventoryItem/InventoryItem'
 import './InventoryList.scss'
 import axios from 'axios'
+import addIcon from '../../Assets/Icons/Icon-add.svg'
+import AddInventory from '../AddInventory/AddInventory'
+import Modal from '../UI/Modal/Modal'
 
-const inventoryURL = "http://localhost:8080/inventory/"
+const inventoryURL = 'http://localhost:8080/inventory/'
 
 class InventoryList extends React.Component {
   constructor() {
     super()
     this.state = {
-      data: []
+      data: [],
+      adding: false,
     }
   }
 
   componentDidMount() {
-    axios.get(inventoryURL)
-      .then(response => {
-        this.setState({
-          data: response.data
-        })
+    axios.get(inventoryURL).then(response => {
+      this.setState({
+        data: response.data,
       })
+    })
+  }
+
+  handleAddingInventory = () => {
+    this.setState({ adding: true })
+  }
+
+  handleCancelAddingInventory = () => {
+    this.setState({ adding: false })
+  }
+
+  handleNewInventory = newInventory => {
+    console.log(newInventory)
   }
 
   render() {
@@ -39,19 +54,38 @@ class InventoryList extends React.Component {
     })
 
     return (
-      <table className="Inventorytable">
-        <tbody>
-          <tr>
-            <th>ITEM</th>
-            <th>LAST ORDERED</th>
-            <th>LOCATION</th>
-            <th>QUANTITY</th>
-            <th>STATUS</th>
-          </tr>
-        </tbody>
+      <>
+        <Modal
+          show={this.state.adding}
+          closeModal={this.handleCancelAddingInventory}
+        >
+          <AddInventory
+            closeModal={this.handleCancelAddingInventory}
+            handleNewInventory={this.handleNewInventory}
+          />
+        </Modal>
+        <table className="Inventorytable">
+          <tbody>
+            <tr>
+              <th>ITEM</th>
+              <th>LAST ORDERED</th>
+              <th>LOCATION</th>
+              <th>QUANTITY</th>
+              <th>STATUS</th>
+            </tr>
+          </tbody>
 
-        <tbody>{rows}</tbody>
-      </table>
+          <tbody>{rows}</tbody>
+        </table>
+        <div className="location__add btn-add">
+          <img
+            className="location__add--btn"
+            src={addIcon}
+            alt="add-button"
+            onClick={this.handleAddingInventory}
+          />
+        </div>
+      </>
     )
   }
 }
